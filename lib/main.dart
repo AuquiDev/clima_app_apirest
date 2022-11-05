58c8c989bd5bfc6cb999201adf0c95537e7474fa
AuquiDev <dev.senior.context@gmail.com>
2022-11-05 11:30:15 -0500

    clima app init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:clima_app_apirest/pages/home_pages.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Clima',
+      home: HomePages()
+    );
+  }
+}
